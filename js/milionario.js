b315ae4f5ebe7aedee07ee15b92f03cc6b87309a
MeggiooGorie <meggioogorie@Peggio.Home>
2018-02-21 19:32:00 +0100

    move data to separata file

--- js/milionario.js
+++ js/milionario.js
@@ -12,92 +12,12 @@ function getRandomArbitrary(min, max) {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-var domande = [
-    [{
-        "domanda": "Quale tra i seguenti prodotti notevoli è una differenza di quadrati?",
-        "risposte": ["(a+b)x²", "(a+b+c)x² $", "π(a+b)ℝ√(x-1X³)", "(a+b)(a-b)"],
-        "corretta": "(a+b)(a-b)",
-        "soluzione": "www.google."
-    }, ], //fine livello 1
-    [{
-        "domanda": "quanto fa 1*1",
-        "risposte": [1, 4, 2, 5],
-        "corretta": 1,
-        "soluzione": "https://www.google.com"
-    }, {
-        "domanda": "dove non si può stare?",
-        "risposte": ["nell'angolo rotture della LIDL", "in sosta vietata", "nelle aule di povo dopo le 6", "sul 5 alla mattina"],
-        "corretta": "nell'angolo rotture della LIDL",
-        "soluzione": "https://www.youtube.com/watch?v=XxvQwSNUeLk"
-    }], //fine livello 2
-    [{
-            "domanda": "qual tra questi è un metallo?",
-            "risposte": ["necromania", "nove", "oro", "AC/DC"],
-            "corretta": "oro",
-            "soluzione": "https://www.google.com"
-        }
-
-
-    ], // fine livello 3
-    [{
-        "domanda": "in che anno sono stati fondati i creed?",
-        "risposte": [1993, 1994, 1992, 15],
-        "corretta": 1993,
-        "soluzione": "https://www.google.com"
-    }, {
-        "domanda": "qual'è stata la canzone di Lady Gaga con il maggior numero di dischi venduti?",
-        "risposte": ["poker face", "paparazzi", "bad romance", "Alejandro"],
-        "corretta": "poker face",
-        "soluzione": "https://www.google.com"
-    }], //fine livello 4
-    [{
-        "domanda": "chi rubò la gioconda nel 1911?",
-        "risposte": ["Alfredo Geri", "Louis Lépine", "Vincenzo Peruggia", "Louis Béroud"],
-        "corretta": "Vincenzo Peruggia",
-        "soluzione": "https://www.google.com"
-    }], //fine livello 5
-    [{
-        "domanda": "quale tra queste è una band christian metal?",
-        "risposte": ["August Burns Red", "pantera", "Iron Claw", "Attila"],
-        "corretta": "August Burns Red",
-        "soluzione": "https://www.google.com"
-    }, {
-        "domanda": "che bottiglia ha tirato su il tini dalle immondizie di mesiano?",
-        "risposte": ["Montenegro", "GinLemon", "Black Crow", "Beera"],
-        "corretta": "GinLemon",
-        "soluzione": "https://www.google.com"
-    }], //fine livello 6
-    [{
-        "domanda": "come si chiamava la prima band del Tini?",
-        "risposte": ["wrathstorm", "the ringhier", "stramezza", "twinkera"],
-        "corretta": "the ringhier",
-        "soluzione": "https://www.google.com"
-    }, {
-        "domanda": "come si chiama l'airband fondata da Ted e l'inserviente in Scrubs?",
-        "risposte": [3, "CoolCats", "Rats", "Hospitalizers"],
-        "corretta": "CoolCats",
-        "soluzione": "https://www.google.com"
-    }], //fine livello 7
-    [{
-        "domanda": "sotto quale imperatore l'impero romano raggiunse la sua massima estensione?",
-        "risposte": ["Claudio", "Nerone", "Traiano", "Augusto"],
-        "corretta": "Traiano",
-        "soluzione": "https://www.google.com"
-    }], //fine livello 8
-    [{
-        "domanda": "chi è stato il fondatore dei Black Death?",
-        "risposte": ["Greg Hicks ", "Mark Shea", "Geezer Butler", "Tommy Thayer"],
-        "corretta": "Greg Hicks ",
-        "soluzione": "https://www.google.com"
-    }], //fine livello 9
-    [{
-        "domanda": "non mi aspettavo arrivassi fin qui",
-        "risposte": [3, 4, 1, 5],
-        "corretta": 1,
-        "soluzione": "https://www.google.com"
-    }], //fine livello 10
-];
+var domade;
+ $.getJSON('./domande_difficili.json', "", function(data){
+   console.log(data);
+  domande =data;
+  start(0);
+}); //domade
 
 function shuffle(a) {
     var j, x, i;
